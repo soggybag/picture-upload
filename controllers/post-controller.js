15aa66116279691de8cf3d6e05adf1e24b389245
Mitchell Hudson <soggybag@gmail.com>
2018-07-09 17:14:47 -0700

    Update post-controller.js

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -56,29 +56,40 @@ module.exports = (app) => {
   // New Post upload image
   // ***************************************
   app.post('/new', (req, res) => {
+    // Check that a file was uploaded. 
     if (!req.files) {
       return res.status(400).send('No files were selected')
     }
 
     console.log("** post new image **");
-
+    
     const body = req.body;
+    // Get the image data from the req.body
     const imageFile = req.files.image;
+    // Split the name on the .
     const fileNameArray = imageFile.name.split('.');
+    // get the file extension
     const fileExtsion = fileNameArray[fileNameArray.length - 1];
+    // generate a short id with the same file extension
     const filePath = `./${shortid.generate()}.${fileExtsion}`;
+    // Define the upload path
     const uploadPath = `./uploads/${filePath}`;
 
+    // Move the uploaded file to the upload path this "saves" the file.
+    // This should move the file to the uploads directory
     imageFile.mv(uploadPath, (err) => {
+      // Check for errors
       if (err) {
         console.log(err);
         return res.status(500)
       }
+      // If no error make a post that includes the path to the file. 
       const post = new Post({
         ...body,
         path: filePath,
         originalFileName: imageFile.name
       });
+      // Save the post to the database. 
       post.save().then((post) => {
         console.log("post added");
         res.redirect('/');
